e4c714cbd4e68350dafb5991fee56dfe2c3d8e01
Raymond <46539130+RayTurk@users.noreply.github.com>
2026-03-02 18:12:32 -0500

    sitemap fixing

--- portfolio/next-sitemap.config.js
+++ portfolio/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://2k26portfolio.netlify.app',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://rturk.me',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'weekly',
